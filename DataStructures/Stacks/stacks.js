05b221259395d9e11e1caa06b3eac43d6ad46474
suyashsrivastava25@gmail.com <suyashsrivastava25@gmail.com>
2020-09-28 08:23:24 +0530

    Stack Implementation

---
+++ DataStructures/Stacks/stacks.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(value) {
+        this.val = value;
+        this.next = null;
+    }
+}
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(value) {
+        let node = new Node(value);
+        if (!this.first) {
+            this.first = node;
+            this.last = node;
+        } else {
+            node.next = this.first;
+            this.first = node;
+        }
+        this.size++;
+        return this;
+    }
+
+    pop() {
+        if (!this.first) return undefined;
+        let node = this.first;
+        this.first = node.next;
+        node.next = null;
+        this.size--;
+        return node;
+    }
+}
+
+let stack = new Stack()
